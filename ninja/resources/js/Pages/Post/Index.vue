9480fc50561a8cd305af7025d609e4ecf556540a
Valentino Bonini <valentino.bonini@alchemer.com>
2022-12-29 09:38:41 -0300

    Initial commit

---
+++ ninja/resources/js/Pages/Post/Index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+	posts: Array,
+});
+</script>
+
+<template>
+	<AppLayout title="Profile">
+		<template #header>
+			<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+				Posts
+			</h2>
+		</template>
+
+		<div>
+			<div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+
+			</div>
+		</div>
+	</AppLayout>
+</template>
